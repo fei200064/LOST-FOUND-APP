b7015394ba4c1db56b1f7dd20e4e389a3a4ab4ca
fei200064 <tanyifei2005@gmail.com>
2025-11-21 16:37:02 +0800

    Upload immage

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // placeholder
+const dropArea=document.getElementById("drop-area");
+const imageUpload=document.getElementById("image-upload");
+const imageView=document.getElementById("img-view");
+
+imageUpload.addEventListener("change",uploadImage1);
+function uploadImage1(){
+    let imgLink=URL.createObjectURL(imageUpload.files[0]);
+    imageView.style.backgroundImage=`url(${imgLink})`;
+    imageView.innerHTML = "";
+}
